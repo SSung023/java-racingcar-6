ba9ef277f0988335c7dff033090ed9bd66aeeb6d
SSung023 <hat7206@naver.com>
2023-12-01 12:51:19 +0900

    feat: 스켈레탈 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceController raceController = new RaceController(
+                new RaceService(), new OutputView(), new InputView()
+        );
+        raceController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,51 @@
+package racingcar.controller;
+
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    private final RaceService raceService;
+    private final OutputView outputView;
+    private final InputView inputView;
+
+
+    public RaceController(RaceService raceService, OutputView outputView, InputView inputView) {
+        this.raceService = raceService;
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        getRaceInfo();
+        playWholeRace();
+        getRaceResults();
+    }
+
+    public void getRaceInfo() {
+        //TODO: 경주차 이름 입력받는 메시지 출력
+
+        //TODO: 사용자로부터 경주차 이름 입력
+
+        //TODO: 시도 횟수를 물어보는 메시지 출력
+
+        //TODO: 사용자로부터 시도 횟수 입력
+
+    }
+
+    public void playWholeRace() {
+        //TODO: 실행 결과 헤더 출력
+
+        //TODO: 각 라운드를 시도 횟수만큼 실행
+
+    }
+
+    public void getRaceResults() {
+        //TODO: 최종 우승자 헤더 출력
+
+        //TODO: RaceService로부터 최종 우승자 전달
+
+        //TODO: OutputView를 통해 우승자 정보 출력
+        
+    }
+}
